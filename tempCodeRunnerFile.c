ab8110c4e5b63a67f17b216c2ed212d1ae4b4391
adistrim <adistrim.dev@gmail.com>
2023-11-17 09:57:47 +0530

    17 Nov 2023

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    if (n%2 == 0 && n != 2)
+    {
+        printf("YES\n");
+    }
+    if (n == 2);
+    {
+        printf("NO\n");
+    }
+    if (n%2 != 0)
+    {
+        printf("NO\n");
+    }
+    
+    return 0;
+}
